d01afbac98148500d44992c41c5692f9cecf8c85
st095229 <suxofructik@gmail.com>
2021-09-17 21:53:47 +0300

    Lesson 1 code upload

---
+++ Project2/Project2/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+
+	cin >> a >> b;
+	c = a + b;
+	cout << a << " + " << b << " = " << c << endl;
+
+	return EXIT_SUCCESS;
+}
